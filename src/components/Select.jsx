f1604d84d647b5f013f4f359d6cd2f1d9a711fbf
lauralen <laurallenkutyte@gmail.com>
2021-02-11 08:11:49 +0200

    Update Select style using SCSS module

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import style from './Select.module.scss';
 
 const Select = ({
   value, name, label, children, onChange,
 }) => (
-  <div className="select-wrapper">
+  <div className={style.selectWrapper}>
     <label htmlFor={name}>{label}</label>
-    <select value={value} name={name} onChange={onChange}>
+    <select
+      className={style.select}
+      value={value}
+      name={name}
+      onChange={onChange}
+    >
       {children}
     </select>
   </div>
